b4ce8e34777ba868c22fd09ad45073b1f3ce01b6
Shahin Alom <shahinalomraj93@gmail.com>
2022-04-05 21:35:05 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/stak_position/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.stak_position
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.deepOrange,
+        ),
+        home: SafeArea(
+          child: Scaffold(
+            floatingActionButton: FloatingActionButton(onPressed: (){},child: Icon(Icons.add),),
+            body: Center(
+              child: Stack(
+                alignment: Alignment.bottomCenter,
+                overflow: Overflow.visible,
+                children: <Widget>[
+                  Container(
+                    height: 850,
+                    width: 500,
+                    color: Colors.amber,
+                  ),
+                  Positioned(
+                    bottom: -50,
+                      child: CircleAvatar(
+                    radius: 50,
+                  ))
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
